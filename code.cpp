f5707eb37fcc5e0c2f026798e194d59163f733a5
Wajahat Iqbal <wajahatiqbalwaji@gmail.com>
2020-02-24 21:16:29 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
 #include<string.h>
 using namespace std;
-
 int main()
 {
      char str[24]="aaabbbbcccfffgggkkkj";
      int n=strlen(str);
-     
-
-   for(int i=1;i<n-1;i++){
-        if( i==1 && str[i-1]!=str[i] 
-             || str[i-1]!=str[i] && str[i]!=str[i+1] 
-             || str[i]!=str[i+1] && i==n-2 ){
+     for(int i=1;i<n-1;i++){
+        if( i==1 && str[i-1]!=str[i] || str[i-1]!=str[i] && str[i]!=str[i+1] || str[i]!=str[i+1] && i==n-2 ){
 		  cout<<i;
-		  break;
-		
-		}
+		  break;}
    }
-   
- 
     return 0;
 }
